dbb58f242d549a8a0bf3084ae818538885d0f794
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-30 15:39:39 +0200

    add class Book and fix class Document

--- Document.cs
+++ Document.cs
@@ -2,19 +2,20 @@
 
 public class Document
 {
-	protected string isbn;
+    public string title;
+    public string author;
+    public DateTime year;
+    public string genre;
+    public bool isAvailable;
+    public int inShelf;
 
-	protected string serialNumber;
-
-	protected string title;
-
-	protected DateOnly year;
-
-	protected string sector;
-
-	protected bool isAvailable;
-
-	public Document()
-	{
-	}
+    public Document(string title, string author, DateTime year, string genre, bool isAvailable, int inShelf)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+        this.inShelf = inShelf;
+    }
 }
