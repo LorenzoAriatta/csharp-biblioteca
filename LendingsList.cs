cbc1ccc0ba19d2058be3d312c33748222bacb8e0
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-30 16:33:02 +0200

    add LendingsList class

---
+++ LendingsList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class LendingsList
+{
+	public List<Lending> lendings;
+
+	public LendingsList()
+	{
+		this.lendings = new List<Lending>();
+	}
+
+	public void NewLending(User onLoan, Document document)
+    {
+
+    }
+}
