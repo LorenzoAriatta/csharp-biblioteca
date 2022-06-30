73e1f9eedcceee584fc8baa44490734a2bd8e5f5
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-30 17:05:59 +0200

    add method for print info

---
+++ UsersList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class UsersList
+{
+	List<User> users;
+
+	public UsersList()
+	{
+		users = new List<User>();
+	}
+
+	public void UserRegistration(string surname, string name, string email, string password, string phone)
+    {
+		User newUser = new User(surname, name, email, password, phone);
+		users.Add(newUser);
+    }
+
+	public void UserInfo()
+    {
+		foreach(User user in users)
+        {
+			Console.WriteLine($"Hello {user.name} {user.surname}! \n Your info: \n email: {user.email} \n phone: {user.phone}");
+		}
+	}
+}
